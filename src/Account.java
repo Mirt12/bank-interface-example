d1abe91d2f4f41e586459860cb224221843d2d4a
Mirt12 <Mirt12@yandex.ru>
2023-05-01 12:25:21 +0200

    add account class

---
+++ src/Account.java
@@ -0,0 +1,18 @@
+import static java.lang.System.out;
+
+public class Account {
+    String name;
+    String address;
+    double balance;
+
+    public void display() {
+        out.print(name);
+        out.print(" (");
+        out.print(") has on account $ ");
+        out.print(balance);
+    }
+
+    public double getInterest(double percentageRate) {
+        return balance * percentageRate / 100.00;
+    }
+}
